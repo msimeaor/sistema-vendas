61a5661849a91c7da624237cfbca56d64a4d724a
Matheus <maatsimeao@gmail.com>
2023-04-26 14:07:04 -0300

    removidas validacoes das DTOs

--- src/main/java/io/github/msimeaor/domain/dtos/ProdutoDto.java
+++ src/main/java/io/github/msimeaor/domain/dtos/ProdutoDto.java
@@ -1,8 +1,5 @@
 package io.github.msimeaor.domain.dtos;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,11 +9,7 @@ import java.math.BigDecimal;
 @Setter
 public class ProdutoDto {
 
-  @NotBlank
-  @Size(max = 100)
   private String descricao;
-
-  @NotNull
   private BigDecimal preco;
 
 }
