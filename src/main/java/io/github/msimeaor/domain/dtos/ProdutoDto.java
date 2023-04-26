267bb984f4cd492759104a9a710342ca2d6d0653
Matheus <maatsimeao@gmail.com>
2023-04-26 14:57:19 -0300

    adicionadas as annotations de validacao na entidade produto e DTO produto

--- src/main/java/io/github/msimeaor/domain/dtos/ProdutoDto.java
+++ src/main/java/io/github/msimeaor/domain/dtos/ProdutoDto.java
@@ -1,5 +1,8 @@
 package io.github.msimeaor.domain.dtos;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +12,11 @@ import java.math.BigDecimal;
 @Setter
 public class ProdutoDto {
 
+  @NotEmpty(message = "O campo descrição é obrigatório!")
   private String descricao;
+
+  @NotNull(message = "O campo preço é obrigatório!")
+  @Digits(integer = 20, fraction = 2)
   private BigDecimal preco;
 
 }
