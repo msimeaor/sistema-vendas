916ed759b1a53ec3b8fddad450e2d0b2f92ce52e
Matheus <maatsimeao@gmail.com>
2023-05-02 17:06:46 -0300

    busca por usuario implementada

--- src/main/java/io/github/msimeaor/domain/repository/Usuarios.java
+++ src/main/java/io/github/msimeaor/domain/repository/Usuarios.java
@@ -2,13 +2,17 @@ package io.github.msimeaor.domain.repository;
 
 import io.github.msimeaor.domain.entily.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface Usuarios extends JpaRepository<Usuario, UUID> {
 
-
+  @Query("select u from Usuario u where u.login = :login")
+  Optional<Usuario> findUsuarioByLogin(@Param("login") String login);
 
 }
