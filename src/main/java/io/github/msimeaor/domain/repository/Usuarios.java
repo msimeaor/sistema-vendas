305979ed712dd05fc360a632d683f0e8eaaaf166
Matheus <maatsimeao@gmail.com>
2023-05-02 16:21:14 -0300

    repositorio usuarios adicionado

---
+++ src/main/java/io/github/msimeaor/domain/repository/Usuarios.java
@@ -0,0 +1,12 @@
+package io.github.msimeaor.domain.repository;
+
+import io.github.msimeaor.domain.entily.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface Usuarios extends JpaRepository<Usuario, UUID> {
+
+}
